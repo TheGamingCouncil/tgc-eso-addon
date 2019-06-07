7e9ae9ec58bd8b08a17205d59baa58725634c850
Steven Adams <steven@adaptivelite.com>
2019-06-06 19:56:23 -0600

    Fixed issue with libmainmenu. Updated version. Removed debug text.

--- guildFeatures/tgcTab.lua
+++ guildFeatures/tgcTab.lua
@@ -130,12 +130,9 @@ function guildRecruitLeaderboardList:BuildMasterList()
   --TGC.rosterDb.invitedHistory = trimList
 
   local currentDateTime = os.date("!*t")
-  d( currentDateTime )
   --each set is from wday 2 - hour 6 
   local day = 60 * 60 * 24
   local currentDayStart = os.time({year=currentDateTime.year, month=currentDateTime.month, day=currentDateTime.day, hour=6, minute=0})
-  d( currentDayStart )
-  d( day )
   local endOfCurrentWeek = 0
   if currentDateTime.wday == 2 then
     if( currentDateTime.hour < 6 ) then
